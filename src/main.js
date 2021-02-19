c3dc1c7057c56c2b49fc1a24a6baca7c0df906f7
Max <yudinmax4444@gmail.com>
2021-02-19 17:51:30 +0200

    roll back to scss

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import store from "./vuex/store"
 import router from "./router/router"
 import "material-design-icons-iconfont"
-import "./assets/styles/styles.css"
+import "./assets/styles/styles.scss"
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
 css: {
     loaderOptions: {
         sass: {
-            prependData: `@import "@/assets/styles/styles.css"`
+            prependData: `@import "@/assets/styles/styles.scss"`
         }
      }
     }
